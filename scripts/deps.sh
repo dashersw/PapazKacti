08d7ee3079e4a772078e3d562e9f12ec303f8059
dashersw <armagan@amcalar.com>
2011-09-15 18:48:29 +0300

    [scripts] Fixed deps.sh paths.

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -3,5 +3,5 @@
 #usage : scripts/deps.sh
 
 js/library/tartJS/tools/goog/build/depswriter.py \
---root_with_prefix='js/ ../../../../' \
+--root_with_prefix='js/ ../../../../../' \
 --output_file='js/deps.js'
