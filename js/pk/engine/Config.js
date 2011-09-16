f3c1b1c6ece802ecf47ca2d2966daea107ba0eb7
dashersw <armagan@amcalar.com>
2011-09-16 03:32:57 +0300

    [config] Implemented the configuration enum for the game.

--- js/pk/engine/Config.js
+++ js/pk/engine/Config.js
@@ -1,4 +1,30 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
+// Copyright 2011 Armagan Amcalar. All Rights Reserved.
 //
-// @author Armagan Amcalar <armagan@tart.com.tr>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Configuration parameters for PapazKacti.
+ */
+
+goog.provide('pk.engine.Config');
+
+
+
+/**
+ * @enum {number}
+ */
+pk.engine.Config = {
+    NUMBER_OF_PLAYERS: 4,
+    HAND_SIZE: 12,
+    NUMBER_OF_DECKS: 2
+};
