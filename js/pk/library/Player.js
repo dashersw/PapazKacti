112849a5330ddc2ab3f9f08bfc54320c52aa049e
dashersw <armagan@amcalar.com>
2011-09-16 03:32:31 +0300

    [library] Introduced the Player class.

--- js/pk/library/Player.js
+++ js/pk/library/Player.js
@@ -1,4 +1,32 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
+// Copyright 2011 Armagan Amcalar. All Rights Reserved.
 //
-// @author Armagan Amcalar <armagan@tart.com.tr>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Player class for PapazKacti.
+ */
+
+goog.provide('pk.Player');
+goog.require('pk.Hand');
+
+
+
+/**
+ * Player class.
+ * @constructor
+ * @param {string=} opt_name Player name. Default is Player + a unique id.
+ */
+pk.Player = function(opt_name) {
+    this.name = opt_name || "Player " + tart.getUid();
+    this.hand = new pk.Hand(pk.engine.Config.HAND_SIZE);
+};
